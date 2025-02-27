14a0b9695b17a59cbb3fc5de59a2e5750e7a6f35
jdmnk <5247767+jdmnk@users.noreply.github.com>
2025-02-27 23:13:04 +0100

    Add "copy" to export dialog

--- src/components/export-config-dialog.tsx
+++ src/components/export-config-dialog.tsx
@@ -1,4 +1,4 @@
-import { Download } from "lucide-react";
+import { Check, Copy, Download } from "lucide-react";
 import { Params } from "@/lib/sfxr/sfxr";
 import {
   Dialog,
@@ -9,32 +9,58 @@ import {
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import { useRef, useState } from "react";
 
 export function ExportConfigDialog({ params }: { params: Params }) {
+  const [exportCopied, setExportCopied] = useState(false);
+  const exportTextAreaRef = useRef<HTMLTextAreaElement>(null);
   const handleExportConfig = () => {
-    const configString = JSON.stringify(params, null, 2);
-    return configString;
+    return JSON.stringify(params, null, 2);
+  };
+
+  const handleCopyExport = async () => {
+    if (exportTextAreaRef.current) {
+      exportTextAreaRef.current.select();
+      await navigator.clipboard.writeText(exportTextAreaRef.current.value);
+      setExportCopied(true);
+      setTimeout(() => setExportCopied(false), 2000); // Reset the msg after 2 seconds
+    }
   };
 
   return (
-    <>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="flex items-center">
-            <Download className="mr-2 h-4 w-4" /> Export Config
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="bg-background text-foreground">
-          <DialogHeader>
-            <DialogTitle>Export Configuration</DialogTitle>
-          </DialogHeader>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="flex items-center">
+          <Download className="mr-2 h-4 w-4" /> Export Config
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-background text-foreground">
+        <DialogHeader>
+          <DialogTitle>Export Configuration</DialogTitle>
+        </DialogHeader>
+        <div className="relative">
           <Textarea
+            ref={exportTextAreaRef}
             value={handleExportConfig()}
             readOnly
-            className="min-h-[200px] bg-muted text-foreground"
+            className="min-h-[200px] bg-muted text-foreground pr-10"
           />
-        </DialogContent>
-      </Dialog>
-    </>
+          <Button
+            size="sm"
+            onClick={handleCopyExport}
+            className="absolute top-2 right-2"
+          >
+            {exportCopied ? (
+              <Check className="h-4 w-4" />
+            ) : (
+              <Copy className="h-4 w-4" />
+            )}
+          </Button>
+        </div>
+        {exportCopied && (
+          <p className="text-sm text-green-500 mt-2">Copied to clipboard!</p>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 }
