22c0b5a9625db37bc00b32e7e4d68769e648dc9f
jdmnk <5247767+jdmnk@users.noreply.github.com>
2025-03-02 17:56:39 +0100

    Hide console in prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" && {
+      exclude: ["error"],
+    },
+  },
 };
 
 export default nextConfig;
